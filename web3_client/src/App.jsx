4ad02b321ccaa2a4e1f9f6369a82eacb2e3581c6
Nathan Teissier <nmteissier@gmail.com>
2023-02-20 07:33:02 +0100

    navbar + routing

--- web3_client/src/App.jsx
+++ web3_client/src/App.jsx
@@ -14,6 +14,9 @@ const App = () => {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/create-campaign" element={<CreateCampaign />} />
+          <Route path="/campaign-details/:id" element={<CampaignDetails />} />
         </Routes>
       </div>
     </div>
